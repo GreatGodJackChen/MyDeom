060f7fddd56dde097469c99ae87d978f81085887
GreatGodJackChen <17828267991@163.com>
2019-08-17 18:52:34 +0800

    3333333333

---
+++ AttributeDeom/Controllers/BaseController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace AttributeDeom.Controllers
+{
+    public class BaseController : Controller
+    {
+    }
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false)]
+    public class UserAuthorizeAttribute : Attribute,IAuthorizationFilter
+    {
+        /// <summary>
+        /// 模块别名，可配置更改
+        /// </summary>
+        public string ModuleAlias { get; set; }
+        /// <summary>
+        /// 权限动作
+        /// </summary>
+        public string OperaAction { get; set; }
+        /// <summary>
+        /// 权限访问控制器参数
+        /// </summary>
+        private string Sign { get; set; }
+
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            //一系列操作
+            if (string.IsNullOrEmpty(ModuleAlias))
+            {
+                context.HttpContext.Response.StatusCode = 401;
+                context.HttpContext.Response.ContentType = "application/json";
+                context.HttpContext.Response.WriteAsync("{\"Code\":401,\"Message\":\"无权访问\"}"); ;
+            }
+            else
+            {
+            }
+        }
+    }
+}
---
+++ AttributeDeom/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AttributeDeom.Models;
+
+namespace AttributeDeom.Controllers
+{
+    public class HomeController : Controller
+    {
+       [UserAuthorize]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [UserAuthorize(ModuleAlias = "sssss")]
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
