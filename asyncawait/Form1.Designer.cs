e6a48e1da745c3cf7641dc15248813d707e0bc8a
GreatGodJackChen <17828267991@163.com>
2019-08-23 18:23:36 +0800

    2222222222

---
+++ asyncawait/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace asyncawait
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(44, 95);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(648, 278);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(61, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "同步";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(165, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "异步";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(265, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "异步";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(61, 66);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 1;
+            this.button4.Text = "异步同步带参awaitt";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_ClickAsync);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(165, 66);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 1;
+            this.button5.Text = "异步在后";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_ClickAsync);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(265, 66);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 1;
+            this.button6.Text = "同时启动认为";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_ClickAsync);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(382, 66);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 1;
+            this.button7.Text = "whenAll";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_ClickAsync);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(482, 66);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 1;
+            this.button8.Text = "for异步";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_ClickAsync);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(597, 66);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 23);
+            this.button9.TabIndex = 1;
+            this.button9.Text = "for同步";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(690, 66);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(75, 23);
+            this.button10.TabIndex = 1;
+            this.button10.Text = "for同步";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+    }
+}
+
---
+++ asyncawait/Form1.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace asyncawait
+{
+    public partial class Form1 : Form
+    {
+        Stopwatch stopwatch = new Stopwatch();
+        public delegate void UpdateTxt(string msg);
+        //定义一个委托变量
+        public UpdateTxt updateTxt;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // 实例化委托
+             updateTxt = new UpdateTxt(UpdateTxtMethod);
+        }
+        public void UpdateTxtMethod(string msg)
+        {
+            richTextBox1.AppendText(msg + "\r\n");
+            richTextBox1.ScrollToCaret();
+            richTextBox1.Refresh();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            Invoke(updateTxt, "程序开始");
+            Test();
+            Invoke(updateTxt, "end");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            Invoke(updateTxt, "程序开始");
+            TestAsync();
+            Invoke(updateTxt, "end");
+        }
+        public void Test()
+        {
+            Thread.Sleep(3000);
+            stopwatch.Stop(); //  停止监视
+            TimeSpan timespan = stopwatch.Elapsed; //  获取当前实例测量得出的总时间
+            Invoke(updateTxt, "同步完成"+timespan.TotalMilliseconds);
+            stopwatch.Start();
+
+        }
+        public async void TestAsync()
+        {
+            await Task.Delay(30000);
+            Invoke(updateTxt, "异步完成");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            Invoke(updateTxt, "程序开始");
+            TestAsync();
+            Test();
+            Invoke(updateTxt, "end");
+        }
+
+        private async void button4_ClickAsync(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            Invoke(updateTxt, "程序开始");
+            await TestStringAsync();
+            Test();
+            richTextBox1.AppendText("end");
+        }
+        public async Task<string> TestStringAsync()
+        {
+            await Task.Delay(3000);
+            stopwatch.Stop();
+            TimeSpan timespan = stopwatch.Elapsed; //  获取当前实例测量得出的总时间
+            Invoke(updateTxt, "异步完成" + timespan.TotalMilliseconds);
+            return "异步完成";
+        }
+
+        private async void button5_ClickAsync(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            Invoke(updateTxt, "程序开始");
+            Test();
+            await TestStringAsync();
+            Invoke(updateTxt, "end");
+        }
+
+        private async void button6_ClickAsync(object sender, EventArgs e)
+        {
+
+            stopwatch.Start();
+            richTextBox1.Clear();
+            Invoke(updateTxt, "程序开始");
+            // BeginInvoke(updateTxt, "程序开始");
+            //var testTask= TestStringAsync();
+            await TestStringAsync();
+            Test();
+            //await testTask;
+            Invoke(updateTxt, "end");
+        }
+
+        private async void button7_ClickAsync(object sender, EventArgs e)
+        {
+            var tasks = new List<Task>();
+            tasks.Add(AAsync());
+            tasks.Add(BAsync());
+            await Task.WhenAll(tasks);
+
+            var tt = "aaa";
+        }
+        public async Task<bool> AAsync()
+        {
+            await Task.Delay(3);
+            return true;
+        }
+        public async Task<bool> BAsync()
+        {
+            await Task.Delay(3);
+            return false;
+        }
+        Nu nu=new Nu { Num=0};
+        private async void button8_ClickAsync(object sender, EventArgs e)
+        {
+            nu.Num = 0;
+            stopwatch.Restart();
+            List<Task> lisTask = new List<Task>();
+            for (int i = 0; i < 20; i++)
+            {
+                lisTask.Add(DoSomething());
+            }
+            await Task.WhenAll(lisTask);
+        }
+        public async Task DoSomething()
+        {
+            Nu nu1 = new Nu { Num = 0 };
+            await Task.Run(() => {
+                for (int i = 0; i < 20000; i++)
+                {
+                    nu1.Num++;
+                }
+            });
+            stopwatch.Stop();
+            TimeSpan timespan = stopwatch.Elapsed; //  获取当前实例测量得出的总时间
+            Invoke(updateTxt, nu1.Num.ToString() + "   " + timespan.TotalMilliseconds);
+            stopwatch.Start();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            nu.Num = 0;
+            stopwatch.Restart();
+            for (int i = 0; i < 400000; i++)
+            {
+                nu.Num++;
+            }
+            stopwatch.Stop();
+            TimeSpan timespan = stopwatch.Elapsed; //  获取当前实例测量得出的总时间
+            Invoke(updateTxt, nu.Num.ToString() + "   " + timespan.TotalMilliseconds);
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            stopwatch.Restart();
+            nu.Num = 0;
+            MyThread myThread = new MyThread();
+            myThread.nu = nu;
+            for (int i = 0; i < 20; i++)
+            {
+                BackgroundWorker bw = new BackgroundWorker();
+                bw.DoWork += new DoWorkEventHandler(DoThread);
+                bw.WorkerSupportsCancellation = true;
+                bw.RunWorkerAsync();
+            }
+        }
+        public void DoThread(object sender, DoWorkEventArgs e)
+        {
+            for (int i = 0; i < 20000; i++)
+            {
+                nu.Add();
+            }
+            stopwatch.Stop();
+            TimeSpan timespan = stopwatch.Elapsed; //  获取当前实例测量得出的总时间
+            Invoke(updateTxt, nu.Num.ToString() + "   " + timespan.TotalMilliseconds);
+            stopwatch.Start();
+        }
+    }
+    class Nu {
+        private int num = 0;
+        public int Num { get=> num; set=>num=value; }
+        public int Add() { return Num++; }
+    }
+
+    class MyThread {
+        public Nu nu { get; set; }
+        public void DoThread(object sender, DoWorkEventArgs e)
+        {
+            for (int i = 0; i < 20000; i++)
+            {
+                nu.Add();
+            }
+        }
+    }
+}
